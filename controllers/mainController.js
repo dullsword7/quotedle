846f31558e3dd07726d0bc17dd1e83c68cff3573
Alan Wu <57293716+dullsword7@users.noreply.github.com>
2023-10-22 16:48:37 -0400

    Finished Guess Verification

--- controllers/mainController.js
+++ controllers/mainController.js
@@ -1,5 +1,10 @@
-const guesses = [];
 const fetch = require('node-fetch');
+const guesses = [];
+const quoteObj = {
+    anime: 'one piece',
+    character: 'jwbrowning',
+    quote: 'I would think that he\'s already seen through your heart, and is simply playing the fool to use you. He yanks on the strings of your cheap pride and you dance like a little marionette.'
+}
 /**
  * GET /
  * Homepage
@@ -7,9 +12,9 @@ const fetch = require('node-fetch');
 
 exports.homepage = async(req, res) => {
     try {
-        const quoteObj = await fetch("https://animechan.xyz/api/random")
-            .then((response) => response.json())
-            .then();
+        // const quoteObj = await fetch("https://animechan.xyz/api/random")
+        //     .then((response) => response.json())
+        //     .then();
 
         const locals = {
             title: 'Quotedle',
