d040fe30b8bc8de1f3d37af8b7636a3bfc95adce
Alan Wu <57293716+dullsword7@users.noreply.github.com>
2023-10-26 08:42:15 -0400

    Finished Hint Tooltips

--- public/scripts/hacker.js
+++ public/scripts/hacker.js
@@ -22,31 +22,54 @@
 // }
 
 function displayHintOne() {
-   var a = document.querySelector("#display-hint-1");
-   var b = document.querySelector("#display-hint-2");
+    var a = document.querySelector("#display-hint-1");
+    var b = document.querySelector("#display-hint-2");
+    var c = document.querySelector(".tooltip-1");
 
-   if (a.style.display !== 'none') {
-         a.style.display = 'none';
-   } else {
-      a.style.display = 'block';
-   }
+    if (a.style.display !== 'none') {
+        a.style.display = 'none';
+        borderFix('10px');
+    } else {
+        a.style.display = 'block';
+        borderFix('10px 10px 0px 0px');
+    }
 
-   if (b.style.display !== 'none') {
-         b.style.display = 'none';
-   }
+    if (b.style.display !== 'none') {
+        b.style.display = 'none';
+    }
+    c.style.visibility = 'hidden';
 }
 
 function displayHintTwo() {
-   var a = document.querySelector("#display-hint-1");
-   var b = document.querySelector("#display-hint-2");
+    var a = document.querySelector("#display-hint-1");
+    var b = document.querySelector("#display-hint-2");
+    var c = document.querySelector(".tooltip-2");
 
-   if (b.style.display !== 'none') {
-         b.style.display = 'none';
-   } else {
-      b.style.display = 'block';
-   }
+    if (b.style.display !== 'none') {
+        b.style.display = 'none';
+        borderFix('10px');
+    } else {
+        b.style.display = 'block';
+        borderFix('10px 10px 0px 0px');
+    }
 
-   if (a.style.display !== 'none') {
-         a.style.display = 'none';
-   }
+    if (a.style.display !== 'none') {
+        a.style.display = 'none';
+    }
+    c.style.visibility = 'hidden';
+}
+
+function borderFix(radius) {
+    var x = document.querySelector(".hint-box");
+    x.style.borderRadius = radius;
+}
+
+function showToolTip(element) {
+    var x = document.getElementById(element);
+    x.style.visibility = 'visible';
+}
+
+function hideToolTip(element) {
+    var x = document.getElementById(element);
+    x.style.visibility = 'hidden';
 }
